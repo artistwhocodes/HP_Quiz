4546369187c4862017860a47991d604f863687c4
github username <github email address>
2019-06-18 12:51:48 -0400

    🐍add founder method & change house method

--- lib/HP_Quiz/slytherin.rb
+++ lib/HP_Quiz/slytherin.rb
@@ -31,38 +31,38 @@ class HPQuiz::Slytherin
   end
 
   def house_infor
-    header = HPQuiz::Scraper.new.header("slytherin")
-    ht = HPQuiz::Scraper.new.header_title("slytherin")
-    hd = HPQuiz::Scraper.new.header_description("slytherin")
+    house_header = HPQuiz::Scraper.new.house_header("slytherin")
+    house_ht = HPQuiz::Scraper.new.house_header_title("slytherin")
+    house_hd = HPQuiz::Scraper.new.house_header_description("slytherin")
     puts "\n"
-    puts Rainbow("❝#{header}❞").green.bright
+    puts Rainbow("❝#{house_header}❞").green.bright
 
-    # puts <<~DOC
-    #  Rainbow(#{ht[0]}).red.bright.underline \n #{hd[0]} \n
-    #  #{ht[1]} \n #{hd[1]} \n
-    #  #{ht[2]} \n #{hd[2]} \n
-    #  #{ht[3]} \n #{hd[3]} \n
-    # DOC
-    puts "\n"
-    puts Rainbow("✨#{ht[0]}:✨").green.bright.underline
-    puts Rainbow("#{hd[0]}")
-    puts "\n"
-    puts Rainbow("✨#{ht[1]}:✨").green.bright.underline
-    puts Rainbow("#{hd[1]}")
-    puts "\n"
-    puts Rainbow("✨#{ht[2]}:✨").green.bright.underline
-    puts Rainbow("#{hd[2]}")
-    puts "\n"
-    puts Rainbow("✨#{ht[3]}:✨").green.bright.underline
-    puts Rainbow("#{hd[3]}")
+    house_infor_section = house_ht.zip(house_hd).to_h
+
+    house_infor_section.each do  |k , v|
+      puts "\n"
+      puts Rainbow("✨#{k}:✨").green.bright.underline
+      puts Rainbow("#{v}")
+    end
 
     exit
-
-    system "clear"
   end
 
   def founder_infor
-    puts "founder stuff"
+    founder_header = HPQuiz::Scraper.new.founder_header("salazar-slytherin")
+    founder_ht = HPQuiz::Scraper.new.founder_header_title("salazar-slytherin")
+    founder_hd = HPQuiz::Scraper.new.founder_header_description("salazar-slytherin")
+    puts "\n"
+    puts Rainbow("❝#{founder_header}❞").green.bright
+
+    founder_infor_section = founder_ht.zip(founder_hd).to_h
+
+    founder_infor_section.each do  |k , v|
+      puts "\n"
+      puts Rainbow("✨#{k}:✨").green.bright.underline
+      puts Rainbow("#{v}")
+    end
+
     exit
   end
 
