36e0f1c44ecaa3dfac932c351ccf27109c3f5c10
github username <github email address>
2019-06-18 00:30:59 -0400

    add hufflepuff🐿 and ravenclaw🦅

--- lib/environment.rb
+++ lib/environment.rb
@@ -2,6 +2,8 @@ require_relative "./HP_Quiz/version"
 require_relative "./HP_Quiz/houses"
 require_relative "./HP_Quiz/gryffindor"
 require_relative "./HP_Quiz/slytherin"
+require_relative "./HP_Quiz/ravenclaw"
+require_relative "./HP_Quiz/hufflepuff"
 
 require_relative "./cli"
 require_relative "./scraper"
