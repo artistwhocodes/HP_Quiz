7e0f86e45e6dde3cbc033aa71fba2b8ea778afdd
github username <github email address>
2019-06-17 22:02:06 -0400

    clean

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,12 +1,27 @@
-require 'open-uri'
-require 'nokogiri'
 class HPQuiz::Scraper
-def get_page(text)
-		doc = Nokogiri::HTML(open("https://www.pottermore.com/#{text}"))
-	end
+  #1st site
+  def get_page(text)
+    doc = Nokogiri::HTML(open("https://www.pottermore.com/#{text}"))
+  end
 
-#1st site
-#With a lion as its crest and Professor McGonagall at its head, Gryffindor is the house which most values the virtues of courage, bravery and determination
+  def banner(house)
+    get_page("collection/all-about-#{house}").search(".subject-description").text.strip
+  end
 
-#comes with sort answer
+  #2nd site
+  def common_room(house)
+    get_page("explore-the-story/#{house}-common-room")
+  end
+
+  def header(house)
+    common_room("#{house}").search(".subject-description").text.strip
+  end
+  #house title
+  def header_title(house)
+    common_room("#{house}").search("article div.l-centered--wide div.l-widget--factfile.factfile dl dt").map(&:text)
+  end
+  #house description
+  def header_description(house)
+    common_room("#{house}").search("article div.l-centered--wide div.l-widget--factfile.factfile dl dd").map(&:text)
+  end
 end
